a9316c03f2faf513d47e1329015e9850aa0c2f11
Nickolay Kondratenko <devmarkup@gmail.com>
2015-11-18 14:37:54 +0200

    add some plugins

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 ROOT=`pwd`
 echo "Starting installation process"
 
-echo "Installing packages"
-source scripts/runnable/install-packages.sh
+#echo "Installing packages"
+#source scripts/runnable/install-packages.sh
 
 echo "Applying symlinks"
 source scripts/runnable/apply-symlinks.sh
 
-echo "Some post-installation magic"
-source scripts/runnable/post-install.sh
-
-echo "Installing monaco font"
-source scripts/runnable/install-monaco-font-ubuntu.sh
+#echo "Some post-installation magic"
+#source scripts/runnable/post-install.sh
+#
+#echo "Installing monaco font"
+#source scripts/runnable/install-monaco-font-ubuntu.sh
